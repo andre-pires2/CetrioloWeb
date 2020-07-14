8df70c0346f60c84fc47a8603a0996c5b19f5d41
Ana Simplicio <asimplicio37@gmail.com>
2020-07-14 01:36:24 -0300

    Validações no get e post

--- src/main/java/dream/team/app/cetrioloweb/controller/AlunoController.java
+++ src/main/java/dream/team/app/cetrioloweb/controller/AlunoController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import dream.team.app.cetrioloweb.dao.AlunoDao;
 import dream.team.app.cetrioloweb.entity.Aluno;
 
@@ -15,34 +16,58 @@ public class AlunoController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Long id = Long.valueOf(req.getParameter("id"));
-		AlunoDao alunoDao = new AlunoDao();
-		Aluno aluno = alunoDao.searchAluno(id);
-		ObjectMapper mapper = new ObjectMapper();
-		String alunoJson = mapper.writeValueAsString(aluno);
-		resp.setContentType("application/json");
-		resp.setCharacterEncoding("UTF-8");
-		resp.setStatus(200);
-		PrintWriter out = resp.getWriter();
-		out.print(alunoJson);
-		out.flush();
+		try {
+			Long id = Long.valueOf(req.getParameter("id"));
+			AlunoDao alunoDao = new AlunoDao();
+			Aluno aluno = alunoDao.searchAluno(id);
+			String alunoJson = "{}";
+			if (aluno != null) {
+				ObjectMapper mapper = new ObjectMapper();
+				alunoJson = mapper.writeValueAsString(aluno);
+			}
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setStatus(200);
+			PrintWriter out = resp.getWriter();
+			out.print(alunoJson);
+			out.flush();
+		} catch (Exception e) {
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setStatus(400);
+			PrintWriter out = resp.getWriter();
+			out.print(e);
+			out.flush();
+			throw e;
+		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		Aluno aluno = mapper.readValue(req.getReader(), Aluno.class);
-		AlunoDao alunoDao = new AlunoDao();
-		alunoDao.createOrUpdateAluno(aluno);
-		String trabalhoJson = mapper.writeValueAsString(aluno);
-		resp.setContentType("application/json");
-		resp.setCharacterEncoding("UTF-8");
-		resp.setStatus(201);
-		String location = req.getServerName() + ":" + req.getServerPort() + req.getContextPath() + "/aluno?id="
-				+ aluno.getId();
-		resp.setHeader("Location", location);
-		PrintWriter out = resp.getWriter();
-		out.print(trabalhoJson);
-		out.flush();
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			Aluno aluno = mapper.readValue(req.getReader(), Aluno.class);
+			AlunoDao alunoDao = new AlunoDao();
+			alunoDao.createOrUpdateAluno(aluno);
+			String alunoJson = mapper.writeValueAsString(aluno);
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setStatus(201);
+			String location = req.getServerName() + ":" + req.getServerPort() + req.getContextPath() + "/aluno?id="
+					+ aluno.getId();
+			resp.setHeader("Location", location);
+			PrintWriter out = resp.getWriter();
+			out.print(alunoJson);
+			out.flush();
+		} catch (Exception e) {
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setStatus(400);
+			PrintWriter out = resp.getWriter();
+			out.print(e);
+			out.flush();
+			throw e;
+		}
 	}
+	
 }
