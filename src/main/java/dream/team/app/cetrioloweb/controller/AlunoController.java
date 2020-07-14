4cd0602cdd990dae99af21e51eee454d5f8e43ec
Ana Simplicio <asimplicio37@gmail.com>
2020-07-14 01:54:24 -0300

    Adiciona método PUT

--- src/main/java/dream/team/app/cetrioloweb/controller/AlunoController.java
+++ src/main/java/dream/team/app/cetrioloweb/controller/AlunoController.java
@@ -70,4 +70,30 @@ public class AlunoController extends HttpServlet {
 		}
 	}
 	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			Long id = Long.valueOf(req.getParameter("id"));
+			ObjectMapper mapper = new ObjectMapper();
+			Aluno alunoInfo = mapper.readValue(req.getReader(), Aluno.class);
+			alunoInfo.setId(id);
+			AlunoDao alunoDao = new AlunoDao();
+			alunoDao.createOrUpdateAluno(alunoInfo);
+			String alunoJson = mapper.writeValueAsString(alunoInfo);
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setStatus(200);
+			PrintWriter out = resp.getWriter();
+			out.print(alunoJson);
+			out.flush();
+		} catch (Exception e) {
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setStatus(400);
+			PrintWriter out = resp.getWriter();
+			out.print(e);
+			out.flush();
+			throw e;
+		}
+	}
 }
